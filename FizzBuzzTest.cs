578c1dae8aa969d89ae66a970c14ecc2df148a79
Alejandro Labra <alejandro.labra@thoughtworks.com>
2018-10-19 17:26:14 -0300

    Refactor test

--- FizzBuzzTest.cs
+++ FizzBuzzTest.cs
@@ -1,33 +1,35 @@
 using Xunit;
 using FizzBuzzKata;
 
-namespace FizzBuzzKata.Testing
+namespace FizzBuzzKata
 {
-    public class FizzTest
-    {       
+    public class FizzBuzzTest
+    {
+        private FizzBuzz _fizzBuzz;
+        public FizzBuzzTest()
+        {
+            _fizzBuzz = new FizzBuzz();
+        }
         [Fact] 
         public void The_number_is_divisible_by_three_then_instead_Fizz(){
-            var fizzBuzz = new FizzBuzz();
             
-            var result = fizzBuzz.Get(3);
+            var result = _fizzBuzz.Get(3);
 
             Assert.Equal("Fizz", result);
         }
 
         [Fact]
         public void The_number_is_divisible_by_five_then_instead_Buzz(){
-            var fizzBuzz = new FizzBuzz();
 
-            var result = fizzBuzz.Get(5);
+            var result = _fizzBuzz.Get(5);
 
             Assert.Equal("Buzz", result);
         }
 
         [Fact]
         public void The_number_is_divisible_by_three_and_five_then_instead_FizzBuzz(){
-            var fizzBuzz = new FizzBuzz();
 
-            var result = fizzBuzz.Get(15);
+            var result = _fizzBuzz.Get(15);
 
             Assert.Equal("FizzBuzz", result);
         }
